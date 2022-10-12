654e0662456d50c87c251472a7d70a8832b84bd3
tanabe1478 <tanabe.1478@gmail.com>
2022-10-12 15:26:54 +0900

    Initial Commit

---
+++ DraggableAnnotation/DraggableAnnotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraggableAnnotationApp.swift
+//  DraggableAnnotation
+//
+//  Created by tanabe.nobuyuki on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct DraggableAnnotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
